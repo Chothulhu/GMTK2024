43e02660c68fc16aad968b539d3433ebb9caa833
Veljko Vračarević <vracarevicveljko@gmail.com>
2024-08-16 23:45:43 +0200

    Enemy + Stomp

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    private int health;
+    private int maxHealth;
+
+    private float height;
+}
