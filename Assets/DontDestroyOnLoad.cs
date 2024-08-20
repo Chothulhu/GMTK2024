036dfd378a72c19e3a6c0050b3c11d2cb13184e2
dajmus <dimitrijegdsc@gmail.com>
2024-08-20 17:37:34 +0200

    ost

---
+++ Assets/DontDestroyOnLoad.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+    public static DontDestroyOnLoad instance; 
+
+        void Awake()
+        {
+            if(instance == null) instance = this;
+            else if (instance != this) Destroy(this.gameObject);
+
+            DontDestroyOnLoad(this);
+        }
+}
